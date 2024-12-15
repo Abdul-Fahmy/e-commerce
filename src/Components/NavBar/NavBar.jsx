b16f903a4c8a711c63452b98035276f1e4b94838
Abdul-Fahmy <abdelrhmanfahmy69@gmail.com>
2024-12-15 20:50:27 +0200

    navBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -10,8 +10,8 @@ export default function NavBar() {
   let { cartInfo, getProductsCart } = useContext(CartContext);
   const { wishListInfo, getWishList } = useContext(WishListContext);
   const [isUserHidden, setIsUserHidden] = useState("hidden");
-  
-  const [isMenuHidden, setIsMenuHidden] = useState('hidden')
+
+  const [isMenuHidden, setIsMenuHidden] = useState("hidden");
   const handleUserOpen = () => {
     setIsUserHidden("visible");
   };
@@ -59,7 +59,7 @@ export default function NavBar() {
               </ul>
               <Link
                 to={"/cart"}
-                className="hidden md:inline-block cart cursor-pointer ml-auto relative"
+                className="hidden lg:inline-block cart cursor-pointer ml-auto relative"
               >
                 <i className="fa-solid fa-cart-shopping text-lg"></i>
                 <div className="cart-counter absolute h-5  w-5 rounded-full right-0 top-0 translate-x-1/2 -translate-y-1/2 bg-green-600 text-white flex justify-center items-center ">
@@ -70,7 +70,10 @@ export default function NavBar() {
                   )}
                 </div>
               </Link>
-              <Link to={"/wishlist"} className="hidden md:inline-block cart cursor-pointer  relative">
+              <Link
+                to={"/wishlist"}
+                className="hidden lg:inline-block cart cursor-pointer  relative"
+              >
                 <i className={` fa-solid fa-heart text-lg text-red-600`}></i>
                 <div className="cart-counter absolute h-5  w-5 rounded-full right-0 top-0 translate-x-1/2 -translate-y-1/2 bg-green-600 text-white flex justify-center items-center ">
                   {wishListInfo === null ? (
@@ -82,7 +85,11 @@ export default function NavBar() {
               </Link>
             </>
           )}
-          <ul className={`hidden md:flex items-center gap-5 ${!token && "ml-auto"}`}>
+          <ul
+            className={`hidden md:flex items-center gap-5 ${
+              !token && "ml-auto"
+            }`}
+          >
             <li>
               <Link>
                 <i className="fa-brands fa-instagram"></i>
@@ -128,79 +135,76 @@ export default function NavBar() {
             )}
             {token && (
               <>
-              <div
-                onClick={() => {
-                  if (isUserHidden === "hidden") {
-                    handleUserOpen();
-                    handleMenuClose()
-                  } else {
-                    handleUserClose();
-                  }
-                }}
-                className=" cursor-pointer w-8 h-8  rounded-full bg-gray-700 text-white flex justify-center items-center relative"
-              >
-                <i className="fa-solid fa-user"></i>
-
                 <div
-                  className={`absolute bg-slate-100 top-10 z-50 w-52 right-0 md:right-0 rounded-md ${isUserHidden} `}
+                  onClick={() => {
+                    if (isUserHidden === "hidden") {
+                      handleUserOpen();
+                      handleMenuClose();
+                    } else {
+                      handleUserClose();
+                    }
+                  }}
+                  className=" cursor-pointer w-8 h-8  rounded-full bg-gray-700 text-white flex justify-center items-center relative"
                 >
-                  <ul className="flex flex-col justify-center items-center gap-3 text-slate-700">
-                    <li className="border-solid border-b border-slate-600 w-full text-center py-2  ">
-                      <Link to={"/wishlist"}>WishList</Link>
-                    </li>
-                    <li className="border-solid border-b border-slate-600 w-full text-center py-2">
-                      <Link to={"/cart"}>Cart</Link>
-                    </li>
-                    <li className="border-solid border-b border-slate-600 w-full text-center py-2">
-                      <Link to={"/changePassword"}>Change your password</Link>
-                    </li>
-                    
-                    <li>
-                      <Link className="mb-3 inline-block " onClick={logOut}>
-                        <i className="fa-solid fa-right-from-bracket text-lg"></i>
-                      </Link>
-                    </li>
-                  </ul>
+                  <i className="fa-solid fa-user"></i>
+
+                  <div
+                    className={`absolute bg-slate-100 top-10 z-50 w-52 right-0 md:right-0 rounded-md ${isUserHidden} `}
+                  >
+                    <ul className="flex flex-col justify-center items-center gap-3 text-slate-700">
+                      <li className="border-solid border-b border-slate-600 w-full text-center py-2  ">
+                        <Link to={"/wishlist"}>WishList</Link>
+                      </li>
+                      <li className="border-solid border-b border-slate-600 w-full text-center py-2">
+                        <Link to={"/cart"}>Cart</Link>
+                      </li>
+                      <li className="border-solid border-b border-slate-600 w-full text-center py-2">
+                        <Link to={"/changePassword"}>Change your password</Link>
+                      </li>
+
+                      <li>
+                        <Link className="mb-3 inline-block " onClick={logOut}>
+                          <i className="fa-solid fa-right-from-bracket text-lg"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-              </div>
-              <div
-              onClick={() => {
-                if (isMenuHidden === "hidden") {
-                  handleMenuOpen();
-                  handleUserClose()
-                } else {
-                  handleMenuClose();
-                }
-              }}
-              className="border-solid border border-slate-400 rounded px-2 py-1 relative md:hidden">
-              <i class="fa-solid fa-bars"></i>
-              <div className={`absolute bg-slate-100 top-10 z-50 w-52 right-0 md:right-0 rounded-md ${isMenuHidden} `}>
-              <ul className="flex flex-col justify-center items-center gap-3 text-slate-700">
-                    <li className="border-solid border-b border-slate-600 w-full text-center py-2  ">
-                      <Link to={"/"}>Home</Link>
-                    </li>
-                    <li className="border-solid border-b border-slate-600 w-full text-center py-2">
-                      <Link to={"/products"}>Products</Link>
-                    </li>
-                    <li className="border-solid border-b border-slate-600 w-full text-center py-2">
-                      <Link to={'/categories'}>
-                      Categories
-                      </Link>
-                    </li>
-                    <li className="border-solid border-b border-slate-600 w-full text-center py-2">
-                      <Link to={'/brands'}>
-                      Brands
-                      </Link>
-                    </li>
-                  
-                    <li>
-                      <Link to={'/allorders'}>
-                      Orders
-                      </Link>
-                    </li>
-                  </ul>
-              </div>
-              </div>
+                <div
+                  onClick={() => {
+                    if (isMenuHidden === "hidden") {
+                      handleMenuOpen();
+                      handleUserClose();
+                    } else {
+                      handleMenuClose();
+                    }
+                  }}
+                  className="border-solid border border-slate-400 rounded px-2 py-1 relative md:hidden"
+                >
+                  <i class="fa-solid fa-bars"></i>
+                  <div
+                    className={`absolute bg-slate-100 top-10 z-50 w-52 right-0 md:right-0 rounded-md ${isMenuHidden} `}
+                  >
+                    <ul className="flex flex-col justify-center items-center gap-3 text-slate-700">
+                      <li className="border-solid border-b border-slate-600 w-full text-center py-2  ">
+                        <Link to={"/"}>Home</Link>
+                      </li>
+                      <li className="border-solid border-b border-slate-600 w-full text-center py-2">
+                        <Link to={"/products"}>Products</Link>
+                      </li>
+                      <li className="border-solid border-b border-slate-600 w-full text-center py-2">
+                        <Link to={"/categories"}>Categories</Link>
+                      </li>
+                      <li className="border-solid border-b border-slate-600 w-full text-center py-2">
+                        <Link to={"/brands"}>Brands</Link>
+                      </li>
+
+                      <li>
+                        <Link to={"/allorders"}>Orders</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </>
             )}
           </ul>
